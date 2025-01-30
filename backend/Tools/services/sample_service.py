23130b0dc912788bdd4aac199e1b4d19d1597d84
tavjo <123273297+tavjo@users.noreply.github.com>
2025-01-30 18:26:02 -0500

    updated docstrings in sample_service; added new agents to sample_retriever

--- backend/Tools/services/sample_service.py
+++ backend/Tools/services/sample_service.py
@@ -19,13 +19,13 @@ logger = logging.getLogger(__name__)
 
 def get_sample_name(sample_metadata: List[dict]) -> str | None:
     """
-    Extract the sample Name from the fetched metadata
+    Extract the sample Name from the fetched metadata.
 
     Args:
-        sample_metadata (list[dict]): The metadata of the sample.
+        sample_metadata (List[dict]): The metadata of the sample.
 
     Returns:
-        str | None: The name of the NHP or None if an error occurred.
+        str | None: The name of the sample or None if an error occurred.
     """
     if sample_metadata:
         json_metadata = sample_metadata[0].get('json_metadata', {})
@@ -40,97 +40,16 @@ def get_sample_name(sample_metadata: List[dict]) -> str | None:
         print("Error: Sample name not found.")
         return None
 
-# def fetch_NHP(nhp_uid: str) -> list[dict] | None:
-#     """
-#     Fetch the NHP metadata for a given term.
-
-#     Args:
-#         nhp_uid (str): The UUID to search for in the database.
-
-#     Returns:
-#         list: A list of results from the database query.
-#     """
-#     try:
-#         query = text("""
-#         SELECT uuid, json_metadata
-#         FROM seek_production.samples
-#         WHERE uuid = :nhp_uid;
-#         """)
-#         nhp_metadata = execute_query(query.bindparams(nhp_uid=nhp_uid))
-#         return nhp_metadata
-#     except Exception as e:
-#         logger.error(f"Error fetching NHP metadata: {e}")
-#         return None
-    
-
-# def retrieve_nhp_name(nhp_uid: str) -> str | None:
-#     """
-#     Retrieve the NHP name for a given term.
-
-#     Args:
-#         nhp_uid (str): The UUID to search for in the database.
-
-#     Returns:
-#         str or None: The name of the non-human primate if found, otherwise None.
-#     """
-#     nhp_metadata = fetch_NHP(nhp_uid)
-#     if nhp_metadata:
-#         return get_NHP_name(nhp_metadata)
-#     else:
-#         return None
-
-# def retrieve_nhp_info(nhp_uid: str) -> list[dict] | None:
-#     """
-#     Retrieve NHP metadata as list containing the dictionary of the NHP metadata.
-
-#     Args:
-#         nhp_uid (str): The UID of the NHP.
-
-#     Returns:
-#         list: The NHP information saved to the file or an empty list if an error occurred.
-#     """
-#     nhp_uid = str(nhp_uid).strip()
-#     nhp_metadata = fetch_NHP(nhp_uid)
-#     if not nhp_metadata:
-#         logger.error("No metadata found.")
-#         return []
-    
-#     for entry in nhp_metadata:
-#         # if name in entry["json_metadata"]["Name"]:
-#         entry['parsed_metadata'] = json.loads(entry['json_metadata'])
-
-#     processed_data = []
-
-#     try:
-#         uuids = [entry['uuid'] for entry in nhp_metadata]
-#         idx = uuids.index(nhp_uid)
-#         metadata_dict = nhp_metadata[idx]['parsed_metadata']
-#         logger.info(metadata_dict)
-#     except json.JSONDecodeError:
-#         print("Error decoding JSON")
-#         metadata_dict = {}
-#         # Process each record and convert to NHPInfo
-#     try:
-#         processed_data.append(metadata_dict)
-#     except ValidationError as ve:
-#         # Log the validation error and skip the record
-#         logger.error(f"Validation error: {ve}")
-
-#     logger.info(processed_data)
-#     # Export data to JSON
-#     try:
-#         return processed_data
-#     except Exception as e:
-#         logger.error(f"Error saving NHP information to JSON: {e}")
-#         return None
     
 def fetch_any_sample(uid: str) -> List[dict] | None:
-    """Fetches the metadata for a given sample UID in the database.
+    """
+    Fetches the metadata for a given sample UID in the database.
+
     Args:
         uid (str): The UID of the sample.
 
     Returns:
-        list[dict]: The metadata for the sample.
+        List[dict] | None: The metadata for the sample or None if not found.
     """
     query = text("""
     SELECT uuid, json_metadata
@@ -148,7 +67,7 @@ def retrieve_sample_info(uid: str) -> List[dict] | None:
         uid (str): The UID of the sample.
 
     Returns:
-        list[dict]: The metadata for the sample.
+        List[dict] | None: A list containing the metadata dictionary for the sample or None if an error occurred.
     """
 
     sample_metadata = fetch_any_sample(uid)
@@ -184,17 +103,16 @@ def retrieve_sample_info(uid: str) -> List[dict] | None:
         logger.error(f"Error saving NHP information to JSON: {e}")
         return None
 
-def fetchChildren(term: str, database_name: str = 'DB_NAME1') -> List[dict]:
+def fetchChildren(term: str) -> List[dict]:
     """
     Fetch the children metadata for a given term from the database.
 
     Args:
         term (str): The UUID of the sample to search for in the database.
-        database_name (str): The name of the database to query. Defaults to 'DB_NAME1'.
 
     Returns:
         List[dict]: A list of dictionaries containing the children metadata.
-                    Returns None if an error occurs during the query.
+                    Returns an empty list if an error occurs during the query.
     """
     try:
         query = text("""
@@ -202,26 +120,25 @@ def fetchChildren(term: str, database_name: str = 'DB_NAME1') -> List[dict]:
         FROM dmac.seek_sample_tree
         WHERE uuid = :term;
         """)
-        children_metadata = execute_query(query.bindparams(term=term), database_name)
+        children_metadata = execute_query(query.bindparams(term=term), 'DB_NAME1')
         return children_metadata
     except Exception as e:
         logger.error(f"Error fetching NHP metadata: {e}")
-        return None
+        return []
 
-def fetch_all_descendants(term: str, database_name: str = 'DB_NAME1') -> List[str]:
+def fetch_all_descendants(term: str) -> List[str]:
     """
     Fetch all descendants of a given sample.
 
     Args:
         term (str): The UUID of the sample to search for in the database.
-        database_name (str): The name of the database to query.
 
     Returns:
-        list[str]: A list of all descendant UUIDs.
+        List[str]: A list of all descendant UUIDs. Returns an empty list if an error occurs.
     """
     try:
         # Fetch the children metadata
-        children_metadata = fetchChildren(term, database_name)
+        children_metadata = fetchChildren(term)
         if not children_metadata:
             return []
 
@@ -247,27 +164,27 @@ def fetch_all_descendants(term: str, database_name: str = 'DB_NAME1') -> List[st
         return []
 
 
-def fetchAllMetadata(term: str, database_name: str = 'DB_NAME1', filter: List[str] = None) -> List[dict]:
+def fetchAllMetadata(term: str, filter: List[str] = None) -> List[dict]:
     """
-    Fetch all metadata for a given term and its descendants.
+    Fetch all metadata for a given term and its descendants. For the argument filter, choose one of the following (not exhaustive but most common):
+            - 'PAV' = patient visit
+            - 'D.IMG' = imaging (i.e. CT, X-ray, etc.)
+            - 'TIS' = tissue collection (i.e. blood draw, bone marrow, etc.)
+            - 'BAC' = bacteria 
+            - 'D.SEQ' = raw sequencing data
+            - 'A.SEQ' = analyzed sequencing data
 
     Args:
         term (str): The UUID of the sample to search for in the database.
-        database_name (str): The name of the database to query.
-        filter (List[str], optional): A list of strings to filter the UUIDs. Choose one of the following (not exhaustive but most common):
-            - 'PAV'
-            - 'D.IMG'
-            - 'TIS'
-            - 'BAC'
-            - 'D.SEQ'
-            - 'A.SEQ'
+        filter (List[str], optional): A list of strings to filter the UUIDs. 
 
     Returns:
         List[dict]: A list of metadata dictionaries for the term and its descendants.
+                    Returns an empty list if an error occurs.
     """
     try:
         # Fetch all descendant UUIDs
-        descendants_uuids = fetch_all_descendants(term, database_name)
+        descendants_uuids = fetch_all_descendants(term)
         if filter:
             # Apply filter to the descendant UUIDs
             descendants_uuids = [child for child in descendants_uuids if any(f in child for f in filter)]
@@ -291,7 +208,7 @@ def fetchAllMetadata(term: str, database_name: str = 'DB_NAME1', filter: List[st
         bind_params = {f"uuid_{i}": uuid for i, uuid in enumerate(descendants_uuids)}
         
         # Execute the query
-        all_metadata = execute_query(query.bindparams(**bind_params), database_name)
+        all_metadata = execute_query(query.bindparams(**bind_params), 'DB_NAME')
         logger.info(f"Fetched metadata for {len(all_metadata)} entries.")
         print(all_metadata)
         return all_metadata
@@ -301,24 +218,24 @@ def fetchAllMetadata(term: str, database_name: str = 'DB_NAME1', filter: List[st
         return []
 
 def add_links(sample_uid: str) -> str:
-   """
-   Constructs a link to the sample's page on the NExtSEEK website or constructs a link to download a protocol associated a sample. 
+    """
+    Constructs a link to the sample's page on the NExtSEEK website or constructs a link to download a protocol associated with a sample.
 
-   Args:
-      sample_uid (str): sample UID that will be used to construct the link
+    Args:
+        sample_uid (str): Sample UID that will be used to construct the link.
 
-   Returns:
-      link (str): link to the NHP's sample page on the NExtSEEK website
-   """
-   if sample_uid and not sample_uid.startswith("P."):
-      sample_uid = str(sample_uid).strip()
-      return f"https://nextseek.mit.edu/seek/sampletree/uid={sample_uid}/"
-   elif sample_uid and sample_uid.startswith("P."):
-      sample_uid = str(sample_uid).strip()
-      return f"https://nextseek.mit.edu/seek/sop/uid={sample_uid}/"
-   else:
-      print("No valid sample UID provided. Returning default link to NExtSEEK website.")
-      return f"https://nextseek.mit.edu/"
+    Returns:
+        str: Link to the sample's page on the NExtSEEK website.
+    """
+    if sample_uid and not sample_uid.startswith("P."):
+        sample_uid = str(sample_uid).strip()
+        return f"https://nextseek.mit.edu/seek/sampletree/uid={sample_uid}/"
+    elif sample_uid and sample_uid.startswith("P."):
+        sample_uid = str(sample_uid).strip()
+        return f"https://nextseek.mit.edu/seek/sop/uid={sample_uid}/"
+    else:
+        print("No valid sample UID provided. Returning default link to NExtSEEK website.")
+        return f"https://nextseek.mit.edu/"
 
 def fetch_protocol(uid: str) -> str | None:
     """
@@ -328,7 +245,7 @@ def fetch_protocol(uid: str) -> str | None:
         uid (str): The UID of the sample.
 
     Returns:
-        str | None: The protocol for the sample or None if no protocol is found.
+        str | None: The protocol link for the sample or None if no protocol is found.
     """
     metadata = retrieve_sample_info(uid)
     protocol_uid = metadata[0]["Protocol"] if metadata[0]["Protocol"] else None
